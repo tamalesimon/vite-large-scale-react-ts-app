cbfd55c7591c7e0c39d813b4f86931a9dee68a5e
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2023-03-29 13:05:18 +0300

    add an item.interface

--- src/items/ItemsList.component.tsx
+++ src/items/ItemsList.component.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { ItemInterface } from "./Item.interface";
 
 export const ItemsListComponent: React.FC<{
-    items: any[]
+    items:ItemInterface[]
 }> = (props) => {
     return (
         <div>
             <h3>Items List</h3>
             <ul>
                 {props.items.map((item) =>{
-                    return <li key={item.index}>{item.name}</li>
+                    return <li key={item.id}>{item.name}</li>
                 })}
             </ul>
         </div>
