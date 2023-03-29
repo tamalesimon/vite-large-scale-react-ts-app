9541aaae0cf6182abd9db248191c067694e223a9
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2023-03-29 03:02:51 +0300

    add class component for itemList

--- src/items/ItemsList.component.tsx
+++ src/items/ItemsList.component.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-export const ItemsListComponent: React.FC<{
-    items: any[]
-}> = (props) => {
-    return (
-        <div>
-            <h3>Items List</h3>
-            <ul>
-                {props.items.map((item) => (
-                    <li key={item.index}>{item.name}</li>
-                ))}
-            </ul>
-        </div>
-    )
+export class ItemsListComponent extends React.Component<{
+    items: any[];
+}> {
+    constructor(props: {
+        items: any[];
+    }) {
+        super(props);
+    }
+
+    render(): React.ReactNode {
+        const { items } = this.props;
+
+        return (
+            <div>
+                <h3>Items List</h3>
+                <ul>
+                    {items.map((item) => (
+                        <li key={item.id}>{item.name}</li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
