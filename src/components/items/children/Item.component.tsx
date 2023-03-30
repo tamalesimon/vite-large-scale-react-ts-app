8476e45be2f5a6150f9ccae5c024c9ffe8d18c01
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2023-03-30 17:00:10 +0300

    add unit testing and configurations

---
+++ src/components/items/children/Item.component.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ItemInterface } from "../../model/items/Item.interface";
+
+type Props = {
+    testId: string,
+    model: ItemInterface,
+    onItemSelect: (item: ItemInterface) => void
+}
+
+export const ItemComponent: React.FC<Props> = ({ testId, model, onItemSelect }) => {
+
+    const getCssClass = (): any => {
+        let css = 'item';
+        if (model.selected) {
+
+        } return css.trim();
+    }
+
+    const handleItemClick = (item: ItemInterface) => {
+        onItemSelect(item);
+    }
+
+    return (
+        <div data-testid={testId} className={getCssClass()} onClick={() => handleItemClick(model)}>
+            <div className="selected-indicator">*</div>
+            <div className="name">{model.name}</div>
+        </div>
+    );
+};
+
+
